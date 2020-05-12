c2c95000632360aa80d613cd91ce68606d74dd7c
Abdullah F. Khan <abdullahkhan0503@gmail.com>
2020-05-12 10:18:41 -0600

    update scene setup
    change camera, lighting, obj material, etc
    
    Signed-off-by: Abdullah F. Khan <abdullahkhan0503@gmail.com>

--- src/js/main.js
+++ src/js/main.js
@@ -10,14 +10,15 @@ function init() {
 
   scene = new THREE.Scene()
 
-  camera = new THREE.PerspectiveCamera(50, ww / wh, 0.1, 10000)
-  camera.position.set(0, 0, 500)
+  camera = new THREE.PerspectiveCamera(45, ww / wh, 0.1, 10000)
+  camera.position.set(0, 50, 500)
+  camera.lookAt(0, -50, 0)
   scene.add(camera)
 
   //Add a light in the scene
   directionalLight = new THREE.DirectionalLight(0xffffff, 0.8)
-  directionalLight.position.set(0, 0, 350)
-  directionalLight.lookAt(new THREE.Vector3(0, 0, 0))
+  directionalLight.position.set(0, -40, 350)
+  directionalLight.lookAt(new THREE.Vector3(0, -40, 0))
   scene.add(directionalLight)
 
   //Load the obj file
@@ -40,15 +41,24 @@ let addZipperInScene = function (object) {
   zipper = object
   //Move the zipper in the scene
   zipper.rotation.x = Math.PI / 2
-  zipper.position.y = 0
-  zipper.position.z = 350
+  zipper.position.y = -100
+  zipper.position.z = 400
   //Go through all children of the loaded object and search for a Mesh
   object.traverse(function (child) {
     //This allow us to check if the children is an instance of the Mesh constructor
     if (child instanceof THREE.Mesh) {
-      child.material.color = new THREE.Color(0XFFFFFF)
+      child.material = new THREE.MeshPhongMaterial({
+        color: 0x8e8e8e,
+        specular: 0x050505,
+        emmisive: 0x2b2b2b,
+        shininess: 100,
+        // map: texture,
+        side: THREE.DoubleSide
+      });
       //Sometimes there are some vertex normals missing in the .obj files, ThreeJs will compute them
       child.geometry.computeVertexNormals()
+
+      child.geometry.center()
     }
   })
   //Add the 3D object in the scene
@@ -67,11 +77,19 @@ function onWindowResize() {
 
 }
 
+let time = 0,
+  delta = 0,
+  clock = new THREE.Clock();
+
 let render = function () {
   requestAnimationFrame(render)
 
+  delta = clock.getDelta();
+  time += delta;
+
+  zipper.position.y = (Math.abs(Math.sin(time)) * 3) + 20;
   //Turn the zipper
-  // zipper.rotation.z += .01;
+  // zipper.rotation.z += .01
 
   renderer.render(scene, camera)
 }
